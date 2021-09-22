6ba9bddfd7b8c1c64d3f33b21821dcc587bb8ba3
ralmayer <llev4n@gmail.com>
2021-09-22 15:25:12 +0400

    ditched r3f in favor of vanilla threejs. reasons: more extensive documentation and community support. let's see how it goes

--- src/App.js
+++ src/App.js
@@ -1,95 +1,39 @@
-import React, { useRef, Suspense } from "react";
-import { Canvas, extend, useFrame, useLoader } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { shaderMaterial, Icosahedron } from "@react-three/drei";
 import glsl from "babel-plugin-glsl/macro";
 import "./App.css";
 
-const settings = {
-  speed: 0.2,
-  density: 1.5,
-  strength: 0.2,
-};
-
-const WaveShaderMaterial = shaderMaterial(
-  // Uniform
-  {
-    uTime: 0,
-    uColor: new THREE.Color(0.0, 0.0, 0.0),
-    uTexture: new THREE.Texture(),
-  },
-  // Vertex Shader
-  glsl`
-    precision mediump float;
- 
-    varying vec2 vUv;
-    varying float vWave;
-
-    uniform float uTime;
-
-    #pragma glslify: snoise3 = require(glsl-noise/simplex/3d);
-
-    void main() {
-      vUv = uv;
-
-      vec3 pos = position;
-      float noiseFreq = 2.0;
-      float noiseAmp = 0.4;
-      vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y, pos.z);
-      pos.z += snoise3(noisePos) * noiseAmp;
-      vWave = pos.z;
-
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);  
-    }
-  `,
-  // Fragment Shader
-  glsl`
-    precision mediump float;
-
-    uniform vec3 uColor;
-    uniform float uTime;
-    uniform sampler2D uTexture;
-
-    varying vec2 vUv;
-    varying float vWave;
-
-    void main() {
-      float wave = vWave * 0.2;
-      vec3 texture = texture2D(uTexture, vUv + wave).rgb;
-      gl_FragColor = vec4(uColor, 1.0); 
-    }
-  `
-);
-
-extend({ WaveShaderMaterial });
-
-const Wave = () => {
-  const ref = useRef();
-  useFrame(({ clock }) => (ref.current.uTime = clock.getElapsedTime()));
-  return (
-    <mesh>
-      <Icosahedron args={[1, 64]}>
-        <waveShaderMaterial ref={ref} uColor="orange" />
-      </Icosahedron>
-    </mesh>
-  );
-};
-
-const Loading = () => <h1>Loading...</h1>;
-
-const Scene = () => {
-  return (
-    <Canvas camera={{ fov: 45, near: 0.1, far: 1000, position: [0, 0, 5] }}>
-      <pointLight position={[10, 10, 10]} />
-      <Suspense fallback={<Loading />}>
-        <Wave />
-      </Suspense>
-    </Canvas>
-  );
-};
-
 function App() {
-  return <Scene></Scene>;
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    ref.current.appendChild(renderer.domElement);
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    var animate = function () {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+    animate();
+  }, []);
+
+  const ref = useRef();
+
+  return <div ref={ref} />;
 }
 
 export default App;
